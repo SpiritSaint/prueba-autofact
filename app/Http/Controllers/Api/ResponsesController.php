53c8e89482d14a4f3d1740d79cc309fe28789c12
Ian Torres <iantorres@outlook.com>
2019-10-17 14:50:48 -0300

    Api rest added.

---
+++ app/Http/Controllers/Api/ResponsesController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Response\StoreRequest;
+use App\Response;
+use Illuminate\Http\Request;
+
+class ResponsesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(
+            Response::all(),
+            200
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        return response()->json(Response::create($request->only([
+            'score',
+            'suggestions',
+            'is_correct'
+        ])), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Response $response)
+    {
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Response $response)
+    {
+        return response()->json($response->update($request->only([
+            'score',
+            'suggestions',
+            'is_correct'
+        ])), 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Response  $response
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Response $response)
+    {
+        $response->delete();
+        return response()->json([], 200);
+    }
+}
